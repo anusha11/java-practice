a29dfbc3823092e22cac1c061b4be09660165802
Anusha Sridhar <anusharao78@gmail.com>
2025-09-12 04:03:56 +0530

    Updating Shopping Cart

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -9,15 +9,21 @@ public class ShoppingCart {
         double total;
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("What item would you like to buy?: ");
         item = scanner.nextLine();
+
         System.out.print("What is the price for each?: ");
         unitPrice = scanner.nextDouble();
+
         System.out.print("How many would you like?: ");
         quantity = scanner.nextInt();
+
         total = unitPrice * quantity;
+
         System.out.println("\nYou have bought " + quantity + " " + item + "/s");
         System.out.println("Your total is $" + total);
+
         scanner.close();
     }
 }
