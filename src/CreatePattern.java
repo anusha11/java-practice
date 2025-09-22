13464b66e58a3bb56a0659c51ed4987be47c8979
Anusha Sridhar <anusharao78@gmail.com>
2025-09-23 04:38:14 +0530

    Pattern Creation using nested for loop

---
+++ src/CreatePattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class CreatePattern {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int row;
+        int column;
+        char symbol;
+
+        System.out.print("Enter the # of rows: ");
+        row = scanner.nextInt();
+
+        System.out.print("Enter the # of columns: ");
+        column = scanner.nextInt();
+
+        System.out.print("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < column; j++){
+                System.out.print(symbol + "  ");
+            }
+            System.out.print("\n");
+        }
+        scanner.close();
+    }
+}
